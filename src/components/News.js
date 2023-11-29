a58235daca9218c01f5701309edd78fe709683e0
Abhishek Prajapati <aprajapati@worldbankgroup.org>
2023-11-29 22:45:36 +0530

    v_33 added common function for next and previous

--- src/components/News.js
+++ src/components/News.js
@@ -29,73 +29,74 @@ export class News extends Component {
     }
   }
 
-  // async updateNews() {
-  //   console.log('updateNews'); // lifecycle method
-  //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-  //   let data = await fetch(url);
-  //   let parsedData = await data.json();
-  //   console.log(url);
-  //   console.log(data);
-  //   console.log(parsedData);
-  //   this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
-
-  // }
-  async componentDidMount() {
-    console.log('componentDidMount'); // lifecycle method
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews() { // making next click 1 digit back
+    console.log('updateNews'); // lifecycle method
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(url);
     console.log(data);
     console.log(parsedData);
     this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    window.scrollTo(0, 0);
+  }
+  async componentDidMount() { // lifecycle method
+    // console.log('componentDidMount'); 
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=1&pageSize=${this.props.pageSize}`;
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(url);
+    // console.log(data);
+    // console.log(parsedData);
+    // this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+
+    this.updateNews();
   }
 
   handlePrevoiusClick = async () => {
     console.log("previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(url);
-    console.log(data);
-    console.log(parsedData);
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false
-    })
-    // this.setState({ page: this.state.page - 1 });
-    // this.updateNews();
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(url);
+    // console.log(data);
+    // console.log(parsedData);
+    // this.setState({
+    //   page: this.state.page - 1,
+    //   articles: parsedData.articles,
+    //   loading: false
+    // })
+    await this.setState({ page: this.state.page - 1 }); // added await in ordr to make next previos work fine
+    this.updateNews();
   }
 
 
   handleNextClick = async () => {
     console.log("Next");
-    let abc = this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize);
-    console.log(this.state.page + 1 + " " + "this.state.page + 1 ");
-    console.log(Math.ceil(this.state.totalResults / this.props.pageSize));
-    console.log(abc);
-    console.log(this.props.pageSize);
+    // let abc = this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize);
+    // console.log(this.state.page + 1 + " " + "this.state.page + 1 ");
+    // console.log(Math.ceil(this.state.totalResults / this.props.pageSize));
+    // console.log(abc);
+    // console.log(this.props.pageSize);
 
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(data);
-      console.log(url);
-      console.log(parsedData);
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false
-      })
-    }
-    // this.setState({ page: this.state.page + 1 });
-    // this.updateNews();
+    // if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1387f1eff2414e709feacba16df5304e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //   this.setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json();
+    //   console.log(data);
+    //   console.log(url);
+    //   console.log(parsedData);
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parsedData.articles,
+    //     loading: false
+    //   })
+    // }
 
-
+    await this.setState({ page: this.state.page + 1 }); // adding await made it work else next button making &page=1 on next click 
+    this.updateNews();
   }
   render() {
     return (
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
             <img src={imgUrl ? imgUrl : "https://cdn.24.co.za/files/Cms/General/d/8506/57b497a6eddb47808c3ecf83f50a069a.jpeg"} className="card-img-top" alt="..." />
             <div className="card-body" style={{ width: '100%', position:'relative' }}>
               <h5 className="card-title">{title ? title : ""}
-                <span className="position-absolute top-0 translate-middle badge rounded-pill badge rounded-pill bg-light text-dark " style={{left:'50%', fontWeight:"400"}}>
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark text-light" style={{left:'50%', fontWeight:"400"}}>
                   <small>
                   {source}
                   </small>
@@ -22,7 +22,7 @@ export class NewsItem extends Component {
               </h5>
               <p className="card-text">{description ? description : ""}</p>
               <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {date ? new Date(date).toGMTString() : ""}</small></p>
-              <a href={newsUrl} target='_blank' className="btn btn-primary btn-sm">Read more</a>
+              <a href={newsUrl} target='_blank' className="btn btn-dark text-light btn-sm">Read more</a>
             </div>
           </div>
         </div>
